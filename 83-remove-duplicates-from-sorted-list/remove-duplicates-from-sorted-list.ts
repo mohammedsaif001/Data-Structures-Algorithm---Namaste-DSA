f4b826da8dc93be3f3bbb9f45f92d3b05dbadc31
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-11-25 10:32:08 +0530

    Time: 1 ms (43.38%) | Memory: 58.9 MB (22.15%) - LeetSync

---
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.ts
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+    let sentinal = new ListNode();
+    sentinal.next = head
+    let curr = sentinal.next;
+
+    if(!head) return head;
+    
+    while(curr && curr.next){
+        if(curr.next.val === curr.val){
+            curr.next = curr.next.next
+        }
+        else{
+            curr = curr.next;
+        }
+    }
+
+    return sentinal.next;
+};
