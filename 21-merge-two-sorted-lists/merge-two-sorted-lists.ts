7a4f1fc8ca6448543515af3e16b8fb8275fcee24
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-11-26 09:46:37 +0530

    Time: 0 ms (100.00%) | Memory: 59 MB (19.91%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.ts
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.ts
@@ -11,6 +11,17 @@
  */
 
 function mergeTwoLists(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+    /* 
+    1. Create a Sentinal Node
+    2. Point the current to this sentinal Node
+    3. Check If l1.val is less than l2.val if yes then point the curr.next to this l1 node
+    4. Now increment the l1
+    5. Then increment the curr
+    6. Do the reverse in else condition
+    7. Iterate this Step 3 to 6 Until both l1 && l2 exist because the comparison should happen between 2 nodes
+    8. Lets say if l1 has lesser nodes than l2 then after all the iteration point the last node of curr.next to the remaining of l2 as its already sorted & vice-versa
+    9. Now Return the sentinal.next 
+    */
     let sentinal = new ListNode();
     let curr = sentinal
 
@@ -22,10 +33,10 @@ function mergeTwoLists(l1: ListNode | null, l2: ListNode | null): ListNode | nul
             curr.next = l2;
             l2 = l2.next;
         }
-         curr = curr.next
+        curr = curr.next
     }
-    if(!l1) curr.next = l2
-    if(!l2) curr.next = l1
+    if (!l1) curr.next = l2
+    if (!l2) curr.next = l1
 
     return sentinal.next;
 };
