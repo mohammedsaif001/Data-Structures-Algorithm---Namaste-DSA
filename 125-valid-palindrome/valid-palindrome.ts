db653039e139e0961d6cc976495ffb57f1baee75
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-11-27 13:49:52 +0530

    Time: 9 ms (19.02%) | Memory: 62.3 MB (18.27%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.ts
@@ -0,0 +1,23 @@
+function isPalindrome(s: string): boolean {
+    s = s.toLowerCase();
+    let i = 0;
+    let j = s.length - 1;
+
+    while(i<j){
+        if(!s[i].match(/[a-z0-9]/i)){
+            i++;
+        }
+        else if(!s[j].match(/[a-z0-9]/i)){
+            j--;
+        }
+        else if(s[j]===s[i]){
+            i++;
+            j--;
+        }
+        else{
+            return false;
+        }
+    }
+
+    return true
+};
