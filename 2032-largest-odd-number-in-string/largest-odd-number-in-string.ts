fb791929eb825fd7c0e69a626f32677b9ee60940
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-11-27 15:33:54 +0530

    Time: 200 ms (5.06%) | Memory: 65.4 MB (5.06%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.ts
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.ts
@@ -1,11 +1,9 @@
 function largestOddNumber(num: string): string {
-    let str = ``
     for(let i=num.length-1;i>=0;i--){
         console.log(Number(num[i]))
         if(Number(num[i])%2!==0) {
-            str = num.slice(0,i+1);
-            break;
+          return num.slice(0,i+1);
         }
     }
-    return str
+    return ''
 };
