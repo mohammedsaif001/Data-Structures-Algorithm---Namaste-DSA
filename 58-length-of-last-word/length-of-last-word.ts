557d41676eadbe05a797b796a90be1c62cd17b03
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-11-27 09:35:53 +0530

    Time: 0 ms (100.00%) | Memory: 54.2 MB (84.18%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.ts
@@ -0,0 +1,10 @@
+function lengthOfLastWord(s: string): number {
+    let count = 0;
+    for(let i=s.length-1;i>=0;i--){
+        if(s[i]!==' '){
+            count++;
+        }
+        else if(count!==0 && s[i]===' ') break
+    }
+    return count
+};
