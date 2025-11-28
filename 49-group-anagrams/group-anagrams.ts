2d3427175eb429e66d033a97711e93d9dcf4193c
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-11-28 12:26:13 +0530

    Time: 33 ms (49.46%) | Memory: 70.4 MB (14.46%) - LeetSync

--- 49-group-anagrams/group-anagrams.ts
+++ 49-group-anagrams/group-anagrams.ts
@@ -1,15 +1,37 @@
+// function groupAnagrams(strs: string[]): string[][] {
+//     let obj = {}
+//     for (let i = 0; i < strs.length; i++) {
+//         let word = strs[i];
+//         let sortedWord = word.split('').sort().join('');
+
+//         if (!obj[sortedWord]) {
+//             obj[sortedWord] = [word]
+//         } else {
+//            obj[sortedWord].push(word)
+//         }
+//     }
+//     return Object.values(obj)
+// };
+
+// Optimised Sultion below as the above will take too much time for sorting inbuild 
 function groupAnagrams(strs: string[]): string[][] {
     let obj = {}
     for (let i = 0; i < strs.length; i++) {
+        let arr = new Array(26).fill(0)
         let word = strs[i];
-        let sortedWord = word.split('').sort().join('');
-        let arr = []
-        if (!obj[sortedWord]) {
-            obj[sortedWord] = [word]
-        } else {
-           obj[sortedWord].push(word)
-        }
-    }
 
+        for(let j=0;j<word.length;j++){
+            let index = word[j].charCodeAt(0) - 'a'.charCodeAt(0)
+            arr[index]++
+        }
+        
+        const key = arr.join('#')
+
+
+        if (!obj[key]) {
+            obj[key] = []
+        } 
+           obj[key].push(word)
+    }
     return Object.values(obj)
 };
