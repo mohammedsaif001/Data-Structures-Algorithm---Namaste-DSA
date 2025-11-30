1c9aab1f8d38a281e2faa813df04ebfeb590683b
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-11-30 21:21:15 +0530

    Time: 4 ms (82.52%) | Memory: 58.9 MB (74.50%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.ts
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.ts
@@ -1,11 +1,16 @@
 function evalRPN(tokens: string[]): number {
     let stack = []
-    let arr = ['+', '-', '*', '/']
+    const map = {
+        '+': (a, b) => b + a,
+        '-': (a, b) => b - a,
+        '*': (a, b) => b * a,
+        '/': (a, b) => Math.trunc(b / a),
+    }
     for (let i = 0; i < tokens.length; i++) {
-        if (arr.includes(tokens[i])) {
+        if (map[tokens[i]]) {
             let a = stack.pop();
             let b = stack.pop();
-            let ans = eval(`${b} ${tokens[i]} ${a}`);
+            let ans = map[tokens[i]](Number(a), Number(b))
             stack.push(Math.trunc(ans))
         } else {
             stack.push(tokens[i]);
@@ -14,3 +19,20 @@ function evalRPN(tokens: string[]): number {
 
     return Number(stack.pop());
 };
+
+// function evalRPN(tokens: string[]): number {
+//     let stack = []
+//     let arr = ['+', '-', '*', '/']
+//     for (let i = 0; i < tokens.length; i++) {
+//         if (arr.includes(tokens[i])) {
+//             let b = stack.pop();
+//             let a = stack.pop();
+//             let ans = eval(`${b} ${tokens[i]} ${a}`);
+//             stack.push(Math.trunc(ans))
+//         } else {
+//             stack.push(tokens[i]);
+//         }
+//     }
+
+//     return Number(stack.pop());
+// };
