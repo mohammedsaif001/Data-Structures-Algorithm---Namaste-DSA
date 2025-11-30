28f0aaa0b93805f592e54f0cca9f1d50dea29492
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-11-30 18:21:07 +0530

    Time: 5 ms (46.21%) | Memory: 58 MB (60.69%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.ts
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.ts
@@ -1,15 +1,32 @@
 function removeOuterParentheses(s: string): string {
-    let ans = '';
-    let len = 0;
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] === '(') {
-            if (len > 0) ans += s[i]
-            ++len;
-        } else {
-            --len;
-            if (len > 0) ans += s[i]
+    let stack = [];
+    let ans = ''
+    for(let i=0;i<s.length;i++){
+        if(s[i]==='('){
+            stack.push(s[i]);
+            if(stack.length>1) ans+=s[i];
+        }else{
+            if(stack.length>1) ans+=s[i];
+            stack.pop();
         }
-
     }
+
     return ans
 };
+
+// Using O(1) Time Complexity
+// function removeOuterParentheses(s: string): string {
+//     let ans = '';
+//     let len = 0;
+//     for (let i = 0; i < s.length; i++) {
+//         if (s[i] === '(') {
+//             if (len > 0) ans += s[i]
+//             ++len;
+//         } else {
+//             --len;
+//             if (len > 0) ans += s[i]
+//         }
+
+//     }
+//     return ans
+// };
