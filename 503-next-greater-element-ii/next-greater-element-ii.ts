5bea6d3eb0d7ba54cc72461394289d2e4882eb9a
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-12-02 10:21:27 +0530

    Time: 8 ms (56.96%) | Memory: 63.1 MB (41.77%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.ts
+++ 503-next-greater-element-ii/next-greater-element-ii.ts
@@ -1,23 +1,47 @@
-function nextGreaterElements(nums: number[]): number[] {
-    // 1st Approach by doubling the array
+// function nextGreaterElements(nums: number[]): number[] {
+//     // 1st Approach by doubling the array
 
-    let arr = new Array(nums.length*2).fill(-1)
-    let doubledArray = [...nums,...nums];
+//     let arr = new Array(nums.length*2).fill(-1)
+//     let doubledArray = [...nums,...nums];
+//     let stack = [];
+    
+//     for(let i=doubledArray.length-1;i>=0;i--){
+//         while(stack.length){
+//             let top = stack[stack.length-1]
+//             if(top<=doubledArray[i]){
+//                 stack.pop();
+//             }else{
+//                 arr[i] = top
+//                 break;
+//             }
+//         }
+
+//         stack.push(doubledArray[i])
+//     }
+
+//     return arr.slice(0,nums.length)
+// };
+
+function nextGreaterElements(nums: number[]): number[] {
+    // 2nd Approach by doubling only the indexes
+
+    let n = nums.length
+    let arr = new Array(n).fill(-1)
     let stack = [];
     
-    for(let i=doubledArray.length-1;i>=0;i--){
+    for(let i=(2*n)-1;i>=0;i--){
         while(stack.length){
             let top = stack[stack.length-1]
-            if(top<=doubledArray[i]){
+            if(top<=nums[i%n]){
                 stack.pop();
             }else{
-                arr[i] = top
+                arr[i%n] = top
                 break;
             }
         }
 
-        stack.push(doubledArray[i])
+        stack.push(nums[i%n])
     }
 
-    return arr.slice(0,nums.length)
+    return arr
 };
