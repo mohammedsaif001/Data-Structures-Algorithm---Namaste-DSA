59d3110a6406f0e9650299046586909dbb29ec98
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-12-04 20:58:43 +0530

    Time: 53 ms (8.47%) | Memory: 55.2 MB (44.38%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.ts
@@ -0,0 +1,28 @@
+/**
+ * The knows API is defined in the parent class Relation.
+ * isBadVersion(version: number): boolean {
+ *     ...
+ * };
+ */
+
+var solution = function(isBadVersion: any) {
+
+    return function(n: number): number {
+        let left = 1;
+        let right = n;
+
+        while(left<right){
+            let mid = left + Math.floor((right-left)/2);
+            let isBad = isBadVersion(mid)
+            if(!isBad){
+                left = mid+1;
+            }else{
+              right = mid
+            }
+        }
+
+    return right
+
+    };
+
+};
