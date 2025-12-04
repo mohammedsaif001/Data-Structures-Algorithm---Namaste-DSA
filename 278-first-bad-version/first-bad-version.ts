a2bb9ce88e9e791fcbda276231db9a7e06505d58
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-12-04 21:00:28 +0530

    Time: 44 ms (50.83%) | Memory: 54.9 MB (62.80%) - LeetSync

--- 278-first-bad-version/first-bad-version.ts
+++ 278-first-bad-version/first-bad-version.ts
@@ -5,23 +5,22 @@
  * };
  */
 
-var solution = function(isBadVersion: any) {
+var solution = function (isBadVersion: any) {
 
-    return function(n: number): number {
+    return function (n: number): number {
         let left = 1;
         let right = n;
 
-        while(left<right){
-            let mid = left + Math.floor((right-left)/2);
-            let isBad = isBadVersion(mid)
-            if(!isBad){
-                left = mid+1;
-            }else{
-              right = mid
+        while (left < right) {
+            let mid = left + Math.floor((right - left) / 2);
+            if (!isBadVersion(mid)) {
+                left = mid + 1;
+            } else {
+                right = mid
             }
         }
 
-    return right
+        return right
 
     };
 
