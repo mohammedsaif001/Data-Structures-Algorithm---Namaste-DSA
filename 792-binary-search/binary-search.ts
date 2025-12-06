00e7c5b38cdfe0f3e23346778140781482e3c753
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-12-06 17:59:28 +0530

    Time: 0 ms (100.00%) | Memory: 58.2 MB (80.30%) - LeetSync

--- 792-binary-search/binary-search.ts
+++ 792-binary-search/binary-search.ts
@@ -1,23 +1,38 @@
 function search(nums: number[], target: number): number {
     let left = 0;
-    let right = nums.length -1;
+    let right = nums.length;
+    
+    while(left<=right){
+        let mid = left + Math.floor((right-left)/2);
 
-
-    // if(nums[middle] === target) return middle;
-    // if(nums[middle]>target) return search(nums.slice(0,middle),target)
-    // return search(nums.slice(0),target)
-
-    while(right>=left){
-        let middle = Math.floor((left + right)/2)
-        if(nums[middle] === target) return middle;
-        if(target < nums[middle]){
-            right = middle-1;
-        }
-        else{
-            left = middle + 1;
-        }
+        if(nums[mid]===target) return mid;
+        if(target < nums[mid]) right = mid -1;
+        else left = mid+1
     }
 
     return -1
-
 };
+
+// function search(nums: number[], target: number): number {
+//     let left = 0;
+//     let right = nums.length -1;
+
+
+//     // if(nums[middle] === target) return middle;
+//     // if(nums[middle]>target) return search(nums.slice(0,middle),target)
+//     // return search(nums.slice(0),target)
+
+//     while(right>=left){
+//         let middle = Math.floor((left + right)/2)
+//         if(nums[middle] === target) return middle;
+//         if(target < nums[middle]){
+//             right = middle-1;
+//         }
+//         else{
+//             left = middle + 1;
+//         }
+//     }
+
+//     return -1
+
+// };
