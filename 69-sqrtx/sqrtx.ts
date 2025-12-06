108d08bd3034a0a8a46b913385ddc85e0086b0e8
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-12-06 18:50:46 +0530

    Time: 2 ms (50.82%) | Memory: 57.5 MB (78.78%) - LeetSync

--- 69-sqrtx/sqrtx.ts
+++ 69-sqrtx/sqrtx.ts
@@ -1,24 +1,44 @@
 function mySqrt(x: number): number {
-    // Hint: The SqrRt will be around half of this number so we can use binary search
-
-    if(x<2) return x
+    if(x<2) return x;
     let left = 2;
     let right = Math.floor(x/2);
 
     while(left<=right){
-        let mid = Math.floor(left + ((right-left)/2));
-        let pow =  Math.pow(mid,2)
-        if(x === pow){
-            return mid
-        }
-        else if(pow<x){
-            left = mid+1
-        }else{
-            right = mid -1
-        }
-        
-    }
+        let mid = left + Math.floor((right-left)/2);
+        let pow = Math.pow(mid,2);
 
-    return  right
+        if(pow === x) return mid;
+        if(pow<x) left = mid+1;
+        else right = mid - 1;
+    }
+    return right
+}
+
+
+
+
+
+// function mySqrt(x: number): number {
+//     // Hint: The SqrRt will be around half of this number so we can use binary search
+
+//     if(x<2) return x
+//     let left = 2;
+//     let right = Math.floor(x/2);
+
+//     while(left<=right){
+//         let mid = Math.floor(left + ((right-left)/2));
+//         let pow =  Math.pow(mid,2)
+//         if(x === pow){
+//             return mid
+//         }
+//         else if(pow<x){
+//             left = mid+1
+//         }else{
+//             right = mid -1
+//         }
+        
+//     }
+
+//     return  right
     
-};
+// };
