6ff6476275fd4e29378fd4b87257ff00f44d87f6
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-12-07 10:24:57 +0530

    Time: 0 ms (100.00%) | Memory: 55.9 MB (33.53%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.ts
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.ts
@@ -1,34 +1,25 @@
-function search(a: number[], target: number): number {
+function search(arr: number[], target: number): number {
     let left = 0;
-    let right = a.length - 1;
+    let right = arr.length - 1;
 
     while (left <= right) {
         let mid = left + Math.floor((right - left) / 2);
-
-        if (a[mid] === target) return mid;
-
-        // Right Sorted
-        if (a[mid] <= a[right]) {
-
-            // check if target is inside the right sorted half
-            if (target > a[mid] && target <= a[right]) {
-                left = mid + 1;
+        if (arr[mid] === target) return mid;
+        if (arr[mid] <= arr[right]) { // Check for right sorted array
+            if (arr[mid] < target && target <= arr[right]) {
+                left = mid + 1
             } else {
-                right = mid - 1;
+                right = mid - 1
             }
-
-        } else {
-
-            // Left Sorted
-
-            // check if target is inside the left sorted half
-            if (target < a[mid]  && target >= a[left]) {
+        } else { // Check for left sorted array
+            if (arr[mid] > target && target >= arr[left]) // Lies in left sorted arrray
+            {
                 right = mid - 1;
             } else {
-                left = mid + 1;
+                left = mid + 1
             }
         }
     }
 
-    return -1;
-}
+    return -1
+};                                                                                                                                                                                                                                                                                                                                                                   
