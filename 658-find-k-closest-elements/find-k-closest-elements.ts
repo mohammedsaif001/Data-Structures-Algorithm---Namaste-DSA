ce32b4070e619aded95dfa2e4f42e385d4912f06
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-12-07 13:00:14 +0530

    Time: 0 ms (100.00%) | Memory: 61.9 MB (84.93%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.ts
+++ 658-find-k-closest-elements/find-k-closest-elements.ts
@@ -1,20 +1,15 @@
 function findClosestElements(arr: number[], k: number, x: number): number[] {
-    // 1. Its a sliding window problem
-    // 2. Check if x - left is > mid + 1 if yes then move pointer left as its too far than the element
-
-
     let left = 0;
-    let right = arr.length - k;
+    let right = arr.length - k ;
 
-    while(left<right){
-        let m = left + Math.floor((right-left)/2);
+    while (left < right) {
+        let mid = left + Math.floor((right - left) / 2);
 
-        if((arr[m+k] - x) < (x - arr[m])){
-            left = m + 1
-        }else{
-            right = m;
-        }
+        if ((arr[mid + k] - x) < (x - arr[mid])) left = mid + 1;
+        else right = mid
+
+
     }
 
-    return arr.slice(left,left+k)
+    return arr.slice(left, left + k)
 };
