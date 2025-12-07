462df1e482aba9d40018f91c6631176ae4544346
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-12-07 10:49:03 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (95.85%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.ts
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.ts
@@ -1,22 +1,12 @@
-function findMin(a: number[]): number {
-    // Since its a rotates sorted array we have to find out the element whohc is smaller to its previous element
-    //  Since its sorted wverything should be sorted except for one element return that
+function findMin(arr: number[]): number {
     let left = 0;
-    let right = a.length - 1;
+    let right = arr.length - 1;
 
-    while(left<=right){
-        if(a[left]<=a[right]){
-            return a[left]
-        }
-
-        let mid = left + Math.floor((right-left)/2);
-        if(a[mid]<a[mid-1]) return a[mid];
-        
-        // Check For If its left sorted or right sorted check in the other side of sorted
-        if(a[left]<=a[mid]){
-            left = mid+1;
-        }else{
-            right = mid-1
-        }
+    while (left<right) {
+        let mid = left + Math.floor((right - left) / 2);
+        if (arr[mid] > arr[right]) left = mid + 1;
+        else right = mid; 
     }
+
+    return arr[right]
 };
