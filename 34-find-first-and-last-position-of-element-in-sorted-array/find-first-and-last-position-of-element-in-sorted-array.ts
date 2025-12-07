17bd82ee7293c2defb2c173f813651de7363201a
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-12-07 11:12:19 +0530

    Time: 0 ms (100.00%) | Memory: 56.2 MB (51.98%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.ts
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.ts
@@ -1,35 +1,26 @@
-function searchRange(a: number[], target: number): number[] {
+function searchRange(arr: number[], target: number): number[] {
     let left = 0;
-    let right = a.length - 1;
-    let arr = [-1, -1];
-
-    // Have 2 loops to find left element and right element
+    let right = arr.length - 1;
+    let ans = [-1, -1]
 
     while (left < right) {
         let mid = left + Math.floor((right - left) / 2);
-
-        if (target > a[mid]) {
-            left = mid + 1
-        } else {
-            right = mid;
-        }
+        if (arr[mid] < target) left = mid + 1;
+        else right = mid
     }
 
-    if (a[left] === target) arr[0] = left;
+    if (arr[right] === target) ans[0] = right;
 
     left = 0;
-    right = a.length - 1;
+    right = arr.length - 1;
 
     while (left < right) {
         let mid = left + Math.ceil((right - left) / 2);
-
-        if (target < a[mid]) {
-            right = mid - 1
-        } else {
-            left = mid;
-        }
+        if (arr[mid] <= target) left = mid;
+        else right = mid - 1
     }
-    if (a[left] === target) arr[1] = left;
 
-    return arr
+    if (arr[left] === target) ans[1] = left;
+
+    return ans
 };
