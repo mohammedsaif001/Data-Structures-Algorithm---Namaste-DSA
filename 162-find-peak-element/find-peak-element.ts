fb53b583726b811d512002e172223a72f2fa6185
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-12-07 10:39:03 +0530

    Time: 0 ms (100.00%) | Memory: 55.7 MB (32.54%) - LeetSync

--- 162-find-peak-element/find-peak-element.ts
+++ 162-find-peak-element/find-peak-element.ts
@@ -1,15 +1,12 @@
-function findPeakElement(a: number[]): number {
-  let left = 0;
-  let right = a.length - 1;
+function findPeakElement(arr: number[]): number {
+    let left = 0;
+    let right = arr.length - 1;
 
-  while(left<right){
-    let mid = left + Math.floor((right-left)/2);
-    if(a[mid]<a[mid+1]){
-        left = mid+1
-    }else{
-        right = mid
+    while (left < right) {
+        let mid = left + Math.floor((right - left) / 2);
+        if (arr[mid] < arr[mid + 1]) left = mid + 1;
+        else right = mid
     }
-  }
 
-  return right
+    return right
 };
