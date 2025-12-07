9e234a9bc89a3bd539987d37401840648c2a65f6
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-12-07 11:23:52 +0530

    Time: 0 ms (100.00%) | Memory: 63.9 MB (97.94%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.ts
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.ts
@@ -1,17 +1,13 @@
 function peakIndexInMountainArray(arr: number[]): number {
-    // Since its peak the next element will always be lower so check if a[m] is > a[m-1]
-
     let left = 0;
     let right = arr.length - 1;
 
     while(left<right){
         let mid = left + Math.floor((right-left)/2);
+        if(arr[mid]>arr[mid+1]) right = mid;
+        else left = mid+1
 
-
-    // If the mid is lower than the peak should always be at right
-        if(arr[mid]<arr[mid+1]) left = mid+1;
-        else right = mid;  // If the mid is higher than next element check for max height
     }
 
-    return left
+    return right
 };
