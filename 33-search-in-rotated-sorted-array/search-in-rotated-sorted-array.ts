d9e3b5830b68fefe7dfe65da742541a682b59608
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-12-08 15:02:19 +0530

    Time: 0 ms (100.00%) | Memory: 54.3 MB (87.13%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.ts
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.ts
@@ -4,22 +4,20 @@ function search(arr: number[], target: number): number {
 
     while (left <= right) {
         let mid = left + Math.floor((right - left) / 2);
+
         if (arr[mid] === target) return mid;
-        if (arr[mid] <= arr[right]) { // Check for right sorted array
+
+        if (arr[mid] < arr[right]) {
             if (arr[mid] < target && target <= arr[right]) {
-                left = mid + 1
+                left = mid + 1;
             } else {
-                right = mid - 1
-            }
-        } else { // Check for left sorted array
-            if (arr[mid] > target && target >= arr[left]) // Lies in left sorted arrray
-            {
                 right = mid - 1;
-            } else {
-                left = mid + 1
             }
+        } else {
+            if (arr[mid] > target && target >= arr[left]) right = mid - 1;
+            else left = mid + 1
         }
     }
 
     return -1
-};                                                                                                                                                                                                                                                                                                                                                                   
+};
