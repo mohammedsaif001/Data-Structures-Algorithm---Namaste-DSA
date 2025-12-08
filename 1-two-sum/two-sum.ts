4bc676244760bac4205bfa8f59ef06e482963f30
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-12-08 10:43:31 +0530

    Time: 4 ms (54.81%) | Memory: 57.9 MB (19.12%) - LeetSync

--- 1-two-sum/two-sum.ts
+++ 1-two-sum/two-sum.ts
@@ -1,14 +1,16 @@
-var twoSum = function(nums, target) {
-    let a;
-    let b;
-    for(let i = 0; i < nums.length; i++) {
-        for(let j = i + 1; j < nums.length; j++) {
-            if(nums[i] + nums[j] === target) {
-                a = i;
-                b = j;
-                return [a, b]; // Return immediately after finding the pair
-            }
+function twoSum(arr: number[], target: number): number[] {
+    let map = {}
+
+    // Adding all the elements in the Map
+    for (let i = 0; i < arr.length; i++) {
+        map[arr[i]] = i;
+    }
+
+    // Iterate through array and check the difference if that difference value exists in map & the index is not same then return that index
+    for (let i = 0; i < arr.length; i++) {
+        let diff = target - arr[i];
+        if (diff in map && map[diff] !== i) {
+            return [i, map[diff]]
         }
     }
-    return []; // Return an empty array if no pair is found
 };
