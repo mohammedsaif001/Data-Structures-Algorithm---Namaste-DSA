fa77fecd9bda4452a673d79f5e79fba355fbe352
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-12-10 09:23:24 +0530

    Time: 0 ms (100.00%) | Memory: 55.3 MB (73.56%) - LeetSync

--- 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.ts
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.ts
@@ -1,17 +1,12 @@
 function balancedStringSplit(s: string): number {
-    let temp = 0;
+
     let count = 0;
+    let temp = 0;
 
-    for(let i=0;i<s.length;i++){
-        if(s[i]==='R'){
-            ++temp;
-        }else{
-            --temp;
-        }
-
-        if(temp===0){
-            ++count
-        }
+    for (let i = 0; i<s.length; i++) {
+        if(s[i]==='R') ++temp;
+        else --temp;
+        if(temp===0) ++count
     }
 
     return count;
