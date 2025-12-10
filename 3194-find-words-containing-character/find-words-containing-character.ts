a06b8224fed4aeef4d80a5bab3226b76db9f4800
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-12-10 08:55:36 +0530

    Time: 3 ms (14.85%) | Memory: 59.3 MB (50.99%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.ts
+++ 3194-find-words-containing-character/find-words-containing-character.ts
@@ -1,14 +1,8 @@
 function findWordsContaining(words: string[], x: string): number[] {
-    let res = []
+    let ans = []
     for(let i=0;i<words.length;i++){
-        let currWord = words[i];
-        for(let j=0;j<currWord.length;j++){
-            if(currWord[j] === x){
-                res.push(i)
-                break;
-            }
-        }
+        if(words[i].includes(x)) ans.push(i)
     }
 
-    return res
+    return ans;
 };
