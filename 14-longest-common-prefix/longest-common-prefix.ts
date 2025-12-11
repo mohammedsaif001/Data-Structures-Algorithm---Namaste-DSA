221b8f48b6fe321ca5aee5196119047ac94c73ea
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-12-11 20:02:46 +0530

    Time: 0 ms (100.00%) | Memory: 57 MB (29.94%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.ts
+++ 14-longest-common-prefix/longest-common-prefix.ts
@@ -1,24 +1,16 @@
 function longestCommonPrefix(strs: string[]): string {
-    if (strs.length === 0) return "";
+    if (strs.length === 0) return ''
+    if (strs.length === 1) return strs[0];
 
     let firstWord = strs[0];
-    let subStr = "";
 
     for (let i = 0; i < firstWord.length; i++) {
-        const char = firstWord[i];
-
         for (let j = 1; j < strs.length; j++) {
-            const currWord = strs[j];
-
-            // If index out of bounds or mismatch, return prefix so far
-            if (i >= currWord.length || currWord[i] !== char) {
-                return subStr;
+            if (strs[j][i] !== firstWord[i]) {
+                return firstWord.slice(0, i);
             }
         }
-
-        // All matched at this index → add char
-        subStr += char;
     }
 
-    return subStr;
-}
+    return firstWord; // all matched
+};
