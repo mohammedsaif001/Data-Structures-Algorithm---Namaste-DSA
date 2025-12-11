aaf6ba331f825605c321a9ab33044c31c09c95c0
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-12-11 09:44:54 +0530

    Time: 8 ms (24.24%) | Memory: 62.8 MB (13.43%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.ts
+++ 125-valid-palindrome/valid-palindrome.ts
@@ -1,21 +1,19 @@
 function isPalindrome(s: string): boolean {
-    s = s.toLowerCase();
+    s = s.trim().toLowerCase();
     let i = 0;
     let j = s.length - 1;
-
-    while(i<j){
-        if(!s[i].match(/[a-z0-9]/i)){
-            i++;
-        }
-        else if(!s[j].match(/[a-z0-9]/i)){
-            j--;
-        }
-        else if(s[j]===s[i]){
-            i++;
-            j--;
-        }
-        else{
-            return false;
+    while (i < j) {
+        if (!s[i].match(/[a-z0-9]/i)){ // Check If i pointer is non alphanumeric then skip the curr element
+            ++i;
+        } 
+        else if (!s[j].match(/[a-z0-9]/i)){ // Check If j pointer is non alphanumeric then skip the curr element
+            --j;
+        } 
+        else if (s[i] === s[j]) { // If Both i & j pointer is alphanumeric and both are equal 
+            ++i; --j;
+        } 
+        else {
+            return false
         }
     }
 
