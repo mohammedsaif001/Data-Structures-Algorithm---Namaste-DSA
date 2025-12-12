9c6153a8737af7ebee1fcdb1b2a4f1f19e75532a
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-12-12 20:59:49 +0530

    Time: 2 ms (78.68%) | Memory: 65.9 MB (57.76%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.ts
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.ts
@@ -1,15 +1,12 @@
-function maxProfit(prices: number[]): number {
-    let min = prices[0];
+function maxProfit(arr: number[]): number {
+    let min = arr[0];
     let maxProfit = 0;
 
-    for(let i=1;i<prices.length;i++){
-        if(prices[i]-min > maxProfit){
-            maxProfit = prices[i]-min
-        }
-        
-        if(min>prices[i]){
-            min = prices[i]
-        }
+    for (let i = 1; i < arr.length; i++) {
+        let diff = arr[i] - min
+        if (diff > maxProfit) maxProfit = diff;
+        if (arr[i] < min) min = arr[i]
     }
+
     return maxProfit
 };
