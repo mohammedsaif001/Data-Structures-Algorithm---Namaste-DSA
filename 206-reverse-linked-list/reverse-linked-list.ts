12ccd94830a03691c37462cc31d3c3c02f2e5c77
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-12-13 19:16:28 +0530

    Time: 0 ms (100.00%) | Memory: 58.3 MB (68.96%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.ts
+++ 206-reverse-linked-list/reverse-linked-list.ts
@@ -10,30 +10,15 @@
  * }
  */
 
-/**
- * Reverse a linked list (iterative).
- *
- * Steps happening in each loop:
- * 1. Store next node (temp = curr.next)
- * 2. Reverse pointer (curr.next = prev)
- * 3. Move prev forward (prev = curr)
- * 4. Move curr forward (curr = temp)
- *
- * Logic:
- * - We walk through the list once.
- * - Each node’s next pointer is flipped to point backward.
- * - `prev` ends up as the new head.
- */
- 
 function reverseList(head: ListNode | null): ListNode | null {
     let prev = null;
     let curr = head;
-    
-    while(curr){
+    while (curr) {
         let temp = curr.next;
         curr.next = prev;
         prev = curr;
         curr = temp;
     }
-    return prev
+
+    return prev;
 };
