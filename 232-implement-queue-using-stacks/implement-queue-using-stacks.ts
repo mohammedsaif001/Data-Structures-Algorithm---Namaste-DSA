0e8c6c9dc0f1ba60b31fc907577e84fe87d218d9
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-12-17 10:46:59 +0530

    Time: 0 ms (100.00%) | Memory: 55.1 MB (65.05%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.ts
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.ts
@@ -1,36 +1,50 @@
 class MyQueue {
-    s1: number[];
-    s2: number[];
+    stack1: number[];
+    stack2: number[];
     constructor() {
-        this.s1 = [];
-        this.s2 = []
+        this.stack1 = []
+        this.stack2 = []
     }
 
     push(x: number): void {
-        this.s1.push(x);
+        this.stack1.push(x)
     }
 
     pop(): number {
-        // If s2 is empty, move all elements from s1 to s2
-        if (this.s2.length === 0) {
-            while (this.s1.length > 0) {
-                this.s2.push(this.s1.pop()!);
-            }
+        if (this.stack1.length === 0 && this.stack2.length === 0) return -1;
+
+        if (this.stack2.length) {
+            return this.stack2.pop()
         }
-        return this.s2.pop()!;
+
+        while (this.stack1.length > 1) {
+            this.stack2.push(this.stack1.pop())
+        }
+
+        return this.stack1.pop();
     }
 
     peek(): number {
-        // Same logic as pop but without removing
-        if (this.s2.length === 0) {
-            while (this.s1.length > 0) {
-                this.s2.push(this.s1.pop()!);
-            }
+        if (this.stack1.length === 0 && this.stack2.length === 0) return -1;
+
+        if (this.stack2.length) {
+            return this.stack2[this.stack2.length - 1]
         }
-        return this.s2[this.s2.length - 1];
+
+        while (this.stack1.length > 1) {
+            this.stack2.push(this.stack1.pop())
+        }
+
+        let peek = this.stack1.pop()
+
+        this.stack2.push(peek);
+
+        return peek;
+
     }
+
     empty(): boolean {
-        return this.s1.length === 0 && this.s2.length === 0;
+        return this.stack1.length === 0 && this.stack2.length === 0
     }
 }
 
