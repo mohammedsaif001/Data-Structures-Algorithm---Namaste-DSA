aea8909e90ce76d54db24a8b4d1b48186cd2900c
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-12-17 16:37:05 +0530

    Time: 5 ms (49.45%) | Memory: 58 MB (59.34%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.ts
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.ts
@@ -1,34 +1,34 @@
-function removeOuterParentheses(s: string): string {
-    let stack = [];
-    let ans = ''
+// function removeOuterParentheses(s: string): string {
+//     let stack = [];
+//     let ans = ''
 
+//     for (let char of s) {
+//         if (char === '(') {
+//             stack.push(char);
+//             if (stack.length > 1) ans += char;
+//         }
+//         else {
+//             if (stack.length > 1) ans += char
+//             stack.pop();
+//         }
+//     }
+
+//     return ans
+// };
+
+// Using O(1) Time Complexity
+function removeOuterParentheses(s: string): string {
+    let ans = '';
+    let len = 0;
     for (let char of s) {
         if (char === '(') {
-            stack.push(char);
-            if (stack.length > 1) ans += char;
-        }
-        else {
-            if (stack.length > 1) ans += char
-            stack.pop();
+            if (len > 0) ans += char;
+            ++len;
+        } else {
+            --len;
+            if (len > 0) ans += char
         }
     }
 
-    return ans
+    return ans;
 };
-
-// Using O(1) Time Complexity
-// function removeOuterParentheses(s: string): string {
-//     let ans = '';
-//     let len = 0;
-//     for (let i = 0; i < s.length; i++) {
-//         if (s[i] === '(') {
-//             if (len > 0) ans += s[i]
-//             ++len;
-//         } else {
-//             --len;
-//             if (len > 0) ans += s[i]
-//         }
-
-//     }
-//     return ans
-// };
