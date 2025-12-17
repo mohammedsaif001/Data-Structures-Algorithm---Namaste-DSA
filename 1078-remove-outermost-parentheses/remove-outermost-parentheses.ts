ee42ed42320c382445d871bc51ee157709c5f447
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-12-17 16:32:21 +0530

    Time: 7 ms (17.58%) | Memory: 56.6 MB (90.11%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.ts
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.ts
@@ -1,12 +1,14 @@
 function removeOuterParentheses(s: string): string {
     let stack = [];
     let ans = ''
-    for(let i=0;i<s.length;i++){
-        if(s[i]==='('){
-            stack.push(s[i]);
-            if(stack.length>1) ans+=s[i];
-        }else{
-            if(stack.length>1) ans+=s[i];
+
+    for (let char of s) {
+        if (char === '(') {
+            stack.push(char);
+            if (stack.length > 1) ans += char;
+        }
+        else {
+            if (stack.length > 1) ans += char
             stack.pop();
         }
     }
