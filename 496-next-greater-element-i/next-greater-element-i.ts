ec032f8a3a4be6d8fb216c199f8b5a9b8e6fd148
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-12-21 16:33:55 +0530

    Time: 2 ms (88.78%) | Memory: 58.5 MB (60.75%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.ts
+++ 496-next-greater-element-i/next-greater-element-i.ts
@@ -2,28 +2,25 @@ function nextGreaterElement(nums1: number[], nums2: number[]): number[] {
     let stack = [];
     let map = {}
 
-    // Adding Last Element As the max for that will be -1 as it doesnt exists
     let lastElement = nums2[nums2.length - 1]
+    // Traversing Backwards because we need to find next grater element
     stack.push(lastElement)
+    // Since last elemnt wont have any next greater so -1
     map[lastElement] = -1;
 
     for (let i = nums2.length - 2; i >= 0; i--) {
         while (stack.length) {
-        let top = stack[stack.length - 1];
-            if (top < nums2[i]) {
-                stack.pop();
-            } else {
+            let top = stack[stack.length - 1];
+            if (top > nums2[i]) {
                 map[nums2[i]] = top;
                 break;
+            } else {
+                stack.pop();
             }
         }
 
-        if (stack.length === 0) {
-            map[nums2[i]] = -1;
-        }
+        if (stack.length === 0) map[nums2[i]] = -1;
         stack.push(nums2[i])
-
     }
-
-    return nums1.map((item) => map[item])
+    return nums1.map((item)=>map[item]);
 };
