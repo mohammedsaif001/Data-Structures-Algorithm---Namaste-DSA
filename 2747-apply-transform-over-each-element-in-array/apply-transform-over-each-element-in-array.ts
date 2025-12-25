3e5c21e7431bfbd5d2bccd3b66b1b8f7c93e53ee
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-12-25 08:49:21 +0530

    Time: 31 ms (99.02%) | Memory: 54.3 MB (80.22%) - LeetSync

---
+++ 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.ts
@@ -0,0 +1,9 @@
+function map(arr: number[], fn: (n: number, i: number) => number): number[] {
+    let mappedArr = [];
+
+    for(let i=0;i<arr.length;i++){
+        mappedArr.push(fn(arr[i],i))
+    }
+
+    return mappedArr
+};
