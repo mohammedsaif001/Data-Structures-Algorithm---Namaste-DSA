a175329285327c3cf59d6adc705b957d2f309bea
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2026-01-07 09:50:15 +0530

    Time: 52 ms (27.96%) | Memory: 57.5 MB (75.70%) - LeetSync

---
+++ 2798-chunk-array/chunk-array.ts
@@ -0,0 +1,10 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Obj = Record<string, JSONValue> | Array<JSONValue>;
+
+function chunk(arr: Obj[], size: number): Obj[][] {
+    const res = [];
+    for(let i=0;i<arr.length;i+=size){
+        res.push(arr.slice(i,i+size))
+    }
+    return res;
+};
