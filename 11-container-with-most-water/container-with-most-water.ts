512dc848654a2d3db44d7e2ca4ea0dc192da0bb2
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2026-01-24 10:52:31 +0530

    Time: 0 ms (100.00%) | Memory: 64.2 MB (23.94%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.ts
+++ 11-container-with-most-water/container-with-most-water.ts
@@ -2,15 +2,16 @@ function maxArea(arr: number[]): number {
     let i = 0;
     let j = arr.length - 1;
     let maxArea = 0;
+
     while (i < j) {
-        // Height of lowest line * width of i and j pointers
-        let area = Math.min(arr[i], arr[j]) * (j - i);
-        maxArea = Math.max(area, maxArea)
+        // Height of lowest line * width (j -i)
+        let area = Math.min(arr[i], arr[j]) * (j - i)
+        maxArea = Math.max(area,maxArea)
 
         if (arr[i] < arr[j]) {
-            i++;
+            i++
         } else {
-            j--
+            j--;
         }
     }
 
